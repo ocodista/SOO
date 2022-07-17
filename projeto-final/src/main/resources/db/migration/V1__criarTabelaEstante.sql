89346c7a9c5bd04cd5a6370ba52eb31df216e8de
Caio Borghi <caio@DESKTOP-PE71JR6.localdomain>
2022-07-17 09:18:22 -0300

    Removing unused files

--- projeto-final/src/main/resources/db/migration/V1__criarTabelaEstante.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `estante` (
-	`id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`qtdPrateleiras` int NOT NULL,
-	`qtdNichosPorPrateleira` int NOT NULL,
-    `descricao` VARCHAR(250) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
