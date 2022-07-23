52572b9af37ca9122ef67f086599f59259e5f61a
Caio Borghi <caiohoborghi@gmail.com>
2022-07-23 16:22:21 -0300

    Add label

--- projeto-final/socket-client/src/features/sensor-update/Sensors.js
+++ projeto-final/socket-client/src/features/sensor-update/Sensors.js
@@ -1,79 +1,80 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import {
-  selectSensors,
-} from './sensorsSlice';
-import SensorChart from '../sensor-chart/SensorChart'
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectSensors } from "./sensorsSlice";
+import SensorChart from "../sensor-chart/SensorChart";
 
-const leadingZero = (str) => (str.toString().length === 1 ? `0${str}` : str)
+const leadingZero = (str) => (str.toString().length === 1 ? `0${str}` : str);
 
 const formattedTime = (dateStr) => {
-  let d = dateStr
-  if (typeof (dateStr) !== typeof (Date)) {
-    d = new Date(dateStr)
-  }
+    let d = dateStr;
+    if (typeof dateStr !== typeof Date) {
+        d = new Date(dateStr);
+    }
 
-  let hours = leadingZero(d.getHours())
-  let minutes = leadingZero(d.getMinutes())
-  let seconds = leadingZero(d.getSeconds())
-  return `${hours}:${minutes}:${seconds}`
-}
+    let hours = leadingZero(d.getHours());
+    let minutes = leadingZero(d.getMinutes());
+    let seconds = leadingZero(d.getSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+};
 
 const formattedDate = (dateStr) => {
-  const d = new Date(dateStr)
+    const d = new Date(dateStr);
 
-  let day = leadingZero(d.getDate())
-  let month = leadingZero(d.getMonth())
-  let year = leadingZero(d.getFullYear())
-  let hours = leadingZero(d.getHours())
-  let minutes = leadingZero(d.getMinutes())
-  let seconds = leadingZero(d.getSeconds())
+    let day = leadingZero(d.getDate());
+    let month = leadingZero(d.getMonth());
+    let year = leadingZero(d.getFullYear());
+    let hours = leadingZero(d.getHours());
+    let minutes = leadingZero(d.getMinutes());
+    let seconds = leadingZero(d.getSeconds());
 
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
-}
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+};
 
 function Message({ sentAt, value, label, idx }) {
-  return (
-    <div key={idx}>[{formattedDate(sentAt)}] {value} {label}</div>
-  )
+    return (
+        <div key={idx}>
+            [{formattedDate(sentAt)}] {value} {label}
+        </div>
+    );
 }
 
 function Sensor({ id, category, label, values }) {
-  const name = `Sensor ${id} - ${category} (${label})`
-  const chartData = values.map(({ value, sentAt }) => ({
-    name,
-    value,
-    sentAt: formattedTime(sentAt)
-  }))
+    const name = `Sensor ${id} - ${category} (${label})`;
+    const chartData = values.map(({ value, sentAt }) => ({
+        name,
+        value,
+        sentAt: formattedTime(sentAt),
+    }));
 
-  const renderValues = () => (
-    values.map((value, idx) => <Message {...value} label={label} idx={idx} />)
-  )
+    const renderValues = () =>
+        values.map((value, idx) => (
+            <Message {...value} label={label} idx={idx} />
+        ));
 
-  return (
-    <section className="wh-50" style={{ paddingBottom: 20 }}>
-      <SensorChart 
-        title={name} 
-        data={chartData} 
-        category={category} 
-      />
-    </section>
-  )
+    return (
+        <section className="wh-50" style={{ paddingBottom: 20 }}>
+            <SensorChart title={name} data={chartData} category={category} />
+        </section>
+    );
 }
 
-
 export function Sensors() {
-  const sensors = useSelector(selectSensors);
-  let orderedSensors = [...sensors]
+    const sensors = useSelector(selectSensors);
+    let orderedSensors = [...sensors];
 
-  orderedSensors = orderedSensors.sort((a, b) => a.id - b.id)
+    orderedSensors = orderedSensors.sort((a, b) => a.id - b.id);
 
-  return (
-    <>
-      <h1>Sensores - Estante 1</h1>
-      <div class="wh-100" >
-        {orderedSensors.map((sensor, i) => <Sensor key={i} {...sensor} />)}
-      </div>
-    </>
-  );
+    const renderSensors = () => {
+        if (!sensors.length) return <h2>Nenhum dado foi encontrado...</h2>;
+        return orderedSensors.map((sensor, i) => (
+            <Sensor key={i} {...sensor} />
+        ));
+    };
+
+    return (
+        <>
+            <h1>Sensores</h1>
+            <div class="wh-100">{renderSensors()}</div>
+        </>
+    );
 }
