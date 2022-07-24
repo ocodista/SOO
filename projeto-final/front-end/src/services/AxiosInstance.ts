514d224519f446aa43799054673040a8d5e99ac0
Caio Borghi <caiohoborghi@gmail.com>
2022-07-24 19:09:27 -0300

    Add api baseUrl as variable

--- projeto-final/front-end/src/services/AxiosInstance.ts
+++ projeto-final/front-end/src/services/AxiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
-
+const localIp = '192.168.112.1'
+const baseURL = process.env.NEXT_PUBLIC_API_URL || `http://${localIp}:8080/agrotech`
 export function AxiosInstance () {
   return axios.create({
-    baseURL: 'http://localhost:8080/agrotech',
+    baseURL,
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       'Access-Control-Allow-Origin': '*' // Could work and fix the previous problem, but not in all APIs
